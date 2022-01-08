25d7bc2d8602581c7f0b35e97bc11384f735d0d0
Rados123 <79587683+Rados123@users.noreply.github.com>
2022-01-08 15:09:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('Ustaw nick:')
+appendMessage('Dołączyłeś do czatu')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} dołączył`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} się rozłączył`)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`Ty: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
